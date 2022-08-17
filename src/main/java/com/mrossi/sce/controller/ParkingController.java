b054a697fe62603eb19021ead233ccb2db5dd451
Mateus Rossi <rossijf2@gmail.com>
2022-08-17 17:28:50 -0300

    1st List EndPoint

---
+++ src/main/java/com/mrossi/sce/controller/ParkingController.java
@@ -0,0 +1,31 @@
+package com.mrossi.sce.controller;
+
+import com.mrossi.sce.controller.dto.ParkingDTO;
+import com.mrossi.sce.controller.mapper.ParkingMapper;
+import com.mrossi.sce.model.Parking;
+import com.mrossi.sce.service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
