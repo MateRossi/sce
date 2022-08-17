265f6ea3a04475e378b08e98325d64c5911b84ec
Mateus Rossi <rossijf2@gmail.com>
2022-08-17 16:01:54 -0300

    Initial Commit
    Created project.

---
+++ src/main/java/com/mrossi/sce/SceApplication.java
@@ -0,0 +1,13 @@
+package com.mrossi.sce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SceApplication.class, args);
+    }
+
+}
